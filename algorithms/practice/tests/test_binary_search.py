0bdf645328c86eef2cbb4b908643d80735a85409
Danil Nikitenko <nikitenko638@gmail.com>
2021-06-01 16:02:34 +0300

    algorithms tests completed

---
+++ algorithms/practice/tests/test_binary_search.py
@@ -0,0 +1,13 @@
+import pytest
+import sys
+sys.path.append('/home/geleoz/snap/python-education/algorithms/practice')
+from binary_search import binary_search
+
+
+@pytest.mark.parametrize('test_args, expected', [
+    (((0, 1, 2, 3, 4), 2), 2),
+    (((.0, 1.44, 3.8, 12.5), 12.5), 3),
+    ((('a', 'b', 'c', 'f', 'k'), 'b'), 1)
+])
+def test_binary_search(test_args, expected):
+    assert binary_search(test_args[0], test_args[1]) == expected
---
+++ algorithms/practice/tests/test_quick_sort.py
@@ -0,0 +1,25 @@
+import pytest
+import sys
+sys.path.append('/home/geleoz/snap/python-education/algorithms/practice')
+from iterative_quick_sort import iterative_quick_sort
+
+
+def test_iterative_quick_sort_int():
+    lst1 = lst2 = [4, 2, 4, 11, 50, 32, 3, 0]
+    iterative_quick_sort(lst1)
+    lst2.sort()
+    assert lst1 == lst2
+
+
+def test_iterative_quick_sort_float():
+    lst1 = lst2 = [4.3, 2.1, 4.0, 11.51, 50.9, 32.111, 3.3, .0]
+    iterative_quick_sort(lst1)
+    lst2.sort()
+    assert lst1 == lst2
+
+
+def test_iterative_quick_sort_chars():
+    lst1 = lst2 = ['d', 'a', 'm', 's', 'f', 'a', 'y']
+    iterative_quick_sort(lst1)
+    lst2.sort()
+    assert lst1 == lst2
